c2b051773e2b31104e155538182efaf3f83bdd50
Francis M. Tyers <ftyers@prompsit.com>
2016-12-26 14:12:17 +0000

    simplecov

--- dev/simple-coverage.sh
+++ dev/simple-coverage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-TOTAL=`cat ../texts/*.byv.txt | cut -f2 | sed 's/[:"\.!?;]/ & /g' | sed 's/  */ /g' | tr ' ' '\n' | grep -v '^$' | wc -l`;
-KNOWN=`cat ../texts/*.byv.txt | cut -f2 | sed 's/[:"\.!?;]/ & /g' | sed 's/  */ /g' | tr ' ' '\n' | grep -v '^$' | lt-proc -w ../byv.automorf.bin  | grep -v '\*' | grep '\$ *$' | wc -l`;
+TOTAL=`cat texts/*.byv.txt | cut -f2 | sed 's/[:"\.!?;]/ & /g' | sed 's/  */ /g' | tr ' ' '\n' | grep -v '^$' | wc -l`;
+KNOWN=`cat texts/*.byv.txt | cut -f2 | sed 's/[:"\.!?;]/ & /g' | sed 's/  */ /g' | tr ' ' '\n' | grep -v '^$' | lt-proc -w byv.automorf.bin  | grep -v '\*' | grep '\$ *$' | wc -l`;
 calc "($KNOWN/$TOTAL)*100"
