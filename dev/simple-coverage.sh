a72353179ed914ec0098a89d5383fd0b73803661
Francis M. Tyers <ftyers@prompsit.com>
2016-12-27 23:00:44 +0000

    ls

--- dev/simple-coverage.sh
+++ dev/simple-coverage.sh
@@ -3,3 +3,6 @@
 TOTAL=`cat texts/*.byv.txt | cut -f2 | sed 's/[:"\.!?;]/ & /g' | sed 's/  */ /g' | tr ' ' '\n' | grep -v '^$' | wc -l`;
 KNOWN=`cat texts/*.byv.txt | cut -f2 | sed 's/[:"\.!?;]/ & /g' | sed 's/  */ /g' | tr ' ' '\n' | grep -v '^$' | lt-proc -w byv.automorf.bin  | grep -v '\*' | grep '\$ *$' | wc -l`;
 calc "($KNOWN/$TOTAL)*100"
+TOTAL=`cat texts/compte.byv.txt | cut -f2 | sed 's/[:"\.!?;]/ & /g' | sed 's/  */ /g' | tr ' ' '\n' | grep -v '^$' | wc -l`;
+KNOWN=`cat texts/compte.byv.txt | cut -f2 | sed 's/[:"\.!?;]/ & /g' | sed 's/  */ /g' | tr ' ' '\n' | grep -v '^$' | lt-proc -w byv.automorf.bin  | grep -v '\*' | grep '\$ *$' | wc -l`;
+calc "($KNOWN/$TOTAL)*100"
